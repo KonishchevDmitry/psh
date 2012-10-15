930a9340e78b906526b885c7a1f7ac74b92c632f
Dmitry Konishchev <konishchev@gmail.com>
2012-10-15 12:29:59 +0400

    Tests integrated to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages, setup
+from setuptools.command.test import test as Test
+
+
+class PyTest(Test):
+    def finalize_options(self):
+        Test.finalize_options(self)
+        self.test_args = [ "tests" ]
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
+
 
 with open("README") as readme:
     setup(
@@ -14,4 +27,7 @@ with open("README") as readme:
         author_email = "konishchev@gmail.com",
 
         packages = find_packages(),
+
+        cmdclass = { "test": PyTest },
+        tests_require = [ "pytest" ],
     )
